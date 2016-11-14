e20a1c7a2b234cd2079c34b40db6963935b5e5d5
Ramon Suarez <ramon@betacowork.com>
2016-11-14 18:00:14 +0100

    Fix team page that was changed without my knowledge (lack of memory?)

--- config.rb
+++ config.rb
@@ -30,6 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+data.flats.each do |owner, infos|
+  proxy "/flats/#{owner}.html", "flats/template.html", :locals => { :owner => owner, :infos => infos }, :ignore => true
+end
 
 ###
 # Helpers
